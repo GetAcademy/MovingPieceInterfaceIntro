334e541792568ecd87c4b6e68755d41f90f43d10
Terje Kolderup <terje@kolderup.net>
2020-09-11 08:52:08 +0200

    Initial commit

---
+++ MovingPieceInterfaceIntro/Piece.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MovingPieceInterfaceIntro
+{
+    class Piece
+    {
+        private int _row;
+        private int _col;
+        private int _speedRow;
+        private int _speedCol;
+
+        public void Move()
+        {
+
+        }
+
+        public void Show()
+        {
+            Console.CursorTop = _row;
+            Console.CursorLeft = _col;
+        }
+    }
+}
---
+++ MovingPieceInterfaceIntro/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MovingPieceInterfaceIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
